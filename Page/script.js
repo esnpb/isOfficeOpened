221f763474911af0e764a342a4dcf417858165b5
Karol Laskowski <karol.laskowski@ialbatros.com>
2017-03-05 16:14:51 +0100

    client page files

---
+++ Page/script.js
@@ -0,0 +1,46 @@
+(function() {
+  const TIMEOUT = 10000; // 10 seconds waiting for response, then "NOPE"
+  const $dots = $("#dots");
+  const $subtitle = $("#subtitle");
+
+  var setStatus = function(status, subtitle, dotsInterval) {
+    $subtitle.text(subtitle).show();
+    $("#status").text('NOPE');
+    clearInterval(dotsInterval);
+    $dots.hide();
+  }
+
+  $(document).ready(function() {
+    var dotsInterval = setInterval(function() {
+      if($dots.text().length == 3) {
+        $dots.text('');
+      } else {
+        $dots.text($dots.text() + '.');
+      }
+    }, 200);
+
+    $.ajax({
+      url: "http://212.33.66.221:3003/isOfficeOpened",
+      timeout: TIMEOUT,
+      success: function(result) {
+        var subtitle = "But can be closed in any time.";
+        if (result.timeUntil.length > 0) {
+          try {
+            var time = new Date(result.timeUntil);
+            var hr = time.getHours();
+            var mn = time.getMinutes();
+            subtitle = "Probably until " + (hr < 10 ? "0" + hr : hr) + ":" + (mn < 10 ? "0" + mn : mn);
+          }
+          catch(ex) {
+            // nothing
+          }
+        }
+        setStatus("YUP", subtitle, dotsInterval);
+      },
+      error: function(errror) {
+        var subtitle = "But they may be hiding there...";
+        setStatus("NOPE", subtitle, dotsInterval);
+      },
+    });
+  });
+})();
